206b4d8e4a6baeae2b3c03e257a5f7d4e45f3a84
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2020-03-30 12:50:05 +0100

    Update to use medRxiv

--- run_all.R
+++ run_all.R
@@ -20,38 +20,26 @@ source("R/perform_search.R")
 # Load searching script
 source("R/perform_search.R")
 
-
-
 ###########################################################################
 # Update underlying data sets ---------------------------------------------
 ###########################################################################
+
+# bx_start_node <- 
 
 
 ###########################################################################
 # Perform searches --------------------------------------------------------
 ###########################################################################
-# medRxiv search
-mx_data <-
-  read.csv(
-    paste0(
-      "https://raw.githubusercontent.com/mcguinlu/",
-      "autosynthesis/master/data/",
-      "medRxiv_abstract_list.csv"
-    ), sep = ",",
-    stringsAsFactors = FALSE,
-    fileEncoding = "UTF-8",
-    header = TRUE)
 
-mx_results <- perform_search(query, mx_data)
+# medRxiv search
+
+mx_results <- medrxivr::mx_search(query, from.date = 20200101)
 
 # bioRxiv search
 bx_data <-
   read.csv(
-    paste0(
-      "https://raw.githubusercontent.com/mcguinlu/",
-      "autosynthesis/master/data/",
-      "medRxiv_abstract_list.csv"
-    ), sep = ",",
+    "path/to/file",
+    sep = ",",
     stringsAsFactors = FALSE,
     fileEncoding = "UTF-8",
     header = TRUE)
