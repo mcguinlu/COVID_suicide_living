5ab2400381fb6bb039544d5197b47421b8262dde
Kazeem Olorisade <ja18581@bristol.ac.uk>
2021-04-01 18:38:53 +0100

    Updated search results: 2021-04-01

--- run_all.R
+++ run_all.R
@@ -531,11 +531,7 @@ all_results$ID <- as.numeric(all_results$ID)
 
   
   
-#git add data/results/results_list.csv
-#git add data/results/all_results.csv
-#git add data/results/2021-03-28_results.csv
-#git add data/screening_snapshot/2021-03-28_snapshot.csv
-#git add data/results/timestamp.txt
-#git commit -m "Updated search results: 2021-03-28"
+#git add data/results/results_list.csv && git add data/results/all_results.csv && git add data/results/2021-04-01_results.csv && git add data/screening_snapshot/2021-04-01_snapshot.csv && git add data/results/timestamp.txt 
+#git commit -m "Updated search results: 2021-04-01" 
 #git push -u origin master
   
